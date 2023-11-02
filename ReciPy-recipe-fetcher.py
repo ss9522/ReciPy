5265eb7630d33f58e82a9305bf78fdfe3e15b523
ss9522 <s.sriram.w@gmail.com>
2023-11-01 23:55:37 -0400

    Added file export and refactored HTML parsing.

--- ReciPy-recipe-fetcher.py
+++ ReciPy-recipe-fetcher.py
@@ -1,6 +1,7 @@
 import requests
 from random import choice
 from html.parser import HTMLParser
+from datetime import datetime
 import webbrowser
 
 # <<< Start of class, function, and API construction definitions >>>
@@ -11,12 +12,12 @@ class MyHTMLParser(HTMLParser):  # Subclass inherited from the imported HTMLPars
         self.recording = False  # Not recording anything yet during init.
 
     def handle_starttag(self, tag, attrs):  # Attributes not yet in use but there just in case.
-        if tag == 'li':  # Condition to check for tags starting with <li>.
+        if tag == "li":  # Condition to check for tags starting with <li>.
             self.recording = True  # Set recording flag to begin capturing data inside tag.
-            self.data = ''  # Create empty string for adding content from tag.
+            self.data = ""  # Create empty string for adding content from tag.
 
     def handle_endtag(self, tag):
-        if tag == 'li' and self.recording:  # Condition to check for tags ending </li> and if the parser is already capturing contents inside the <li>example</li>.
+        if tag == "li" and self.recording:  # Condition to check for tags ending </li> and if the parser is already capturing contents inside the <li>example</li>.
             self.recording = False  # Stop recording at end of tag (</li>).
             self.instructions.append(self.data.strip()) # Strip whitespace before/after content of each tag, then append to instructions list.
 
@@ -26,41 +27,40 @@ class MyHTMLParser(HTMLParser):  # Subclass inherited from the imported HTMLPars
 
 def clean_html_instructions(html_content):
 
-    def add_period(instruction):  # Utility function to add full-stop at the end of instruction if it doesn't already have one.
-        instruction = instruction.strip()  # Strips the instruction of any whitespace before/after content.
-        if not instruction.endswith('.'):  # If the instruction does not already end with a full-stop:
-            instruction += '.'  # Add a full-stop
+    def add_period(instruction):  # Inner function to add period to the instruction if not present.
+        instruction = instruction.strip()  # Removing any extra spaces from start and end.
+        if not instruction.endswith("."):
+            instruction += "."  # Add a period if not already there.
         return instruction
-    
-    if "<li>" in html_content:  # If there are <li> tags in the content
-        parser = MyHTMLParser()  # Create object from HTMLParser subclass
-        parser.feed(html_content)  # Feed in HTML content
-        # Ensure each instruction has a full-stop at the end
-        cleaned_instructions_list = [add_period(step) for step in parser.instructions]
-        cleaned_instructions = '\n'.join(f"{index}. {step}" for index, step in enumerate(cleaned_instructions_list, 1))
-    
-    else:  # If there are no <li> tags, split by full-stops.
-        # Split by full-stops and ensure each instruction has a full-stop at the end.
-        instructions_list = [add_period(instr) for instr in html_content.split('.') if instr.strip()]
-        cleaned_instructions = '\n'.join(f"{index}. {step}" for index, step in enumerate(instructions_list, 1))
-    
-    return cleaned_instructions  # Returns enumerated instructions as the function output.
 
+    
+    
+    if any(tag in html_content for tag in ["<li>", "<ol>"]):  # The block inside this condition will execute if <li> or <ol> tags exist in the content.
+        parser = MyHTMLParser()  # Create an instance of the parser.
+        parser.feed(html_content)  # Feed the html content to the parser.
+        cleaned_instructions_list = [add_period(step) for step in parser.instructions]  # Construct list of cleaned instructions after adding period.
+        html_content = ". ".join(cleaned_instructions_list)  # Converting the list into a string for further processing.
+
+    instructions_list = [add_period(instr) for instr in html_content.split(".") if instr.strip()]  # Split by periods and clean each instruction, regardless of whether <li> tags exist.
+    cleaned_instructions = "\n".join(f"{index}. {step}" for index, step in enumerate(instructions_list, 1))  # Joining the instructions to create a single string.
+    cleaned_instructions = cleaned_instructions.replace("<ol>", "").replace("</ol>", "").replace("<p>", "").replace("</p>", "").strip()  # Check and remove the <ol> and </ol> tags if they are present.
+
+    return cleaned_instructions
 
 def get_yes_no_input(prompt):  # Prompt the user for a yes or no input. Returns True for 'y' and False for 'n'.
     
     while True:  # Keeps loop running until exit condition (return) is met.
         choice = str(input(prompt + "\n[Y]es\t[N]o\n>>> ")).lower()  # Parses all inputs as lowercase
-        if len(choice) > 0 and (choice[0] == 'y' or choice[0] == 'n'):
-            return choice[0] == 'y'  # Returns boolean True/False depending on choice being '[y]es'
+        if len(choice) > 0 and (choice[0] == "y" or choice[0] == "n"):
+            return choice[0] == "y"  # Returns boolean True/False depending on choice being '[y]es'
         else:
             print("Invalid input.\n")  # If input doesn't start with 'y' or 'n', keep asking the user until it does (within while True loop).
 
 
 def extract_recipe_information(data):
-    title = data.get('title', '')  # Extracts title from recipe info.
-    ingredients = [ingredient.get('original', '') for ingredient in data.get('extendedIngredients', [])]  # Extracts extended ingredients from API response.
-    html_instructions = data.get('instructions', '')  # Extracts unparsed HTML-formatted instructions, will need further cleaning prior to output.
+    title = data.get("title", "")  # Extracts title from recipe info.
+    ingredients = [ingredient.get("original", "") for ingredient in data.get("extendedIngredients", [])]  # Extracts extended ingredients from API response.
+    html_instructions = data.get("instructions", "")  # Extracts unparsed HTML-formatted instructions, will need further cleaning prior to output.
     instructions = clean_html_instructions(html_instructions) # Call HTML cleaner function with raw HTML instructions as argument
     return title, ingredients, instructions
 
@@ -74,12 +74,33 @@ def display_recipe(title, ingredients, instructions):  # Define function that di
 
 def end_greeting():
     greeting = ["Bon appetit!", "Enjoy the meal!", "Happy cooking!", "*Chef's kiss*"]
-    print("\n" + choice(greeting))
-    visit_site = get_yes_no_input("\nWould you like to visit Spoonacular to find more recipes? [Y]es/[N]o\n>>> ")
+    visit_site = get_yes_no_input("\nWould you like to visit Spoonacular to find more recipes?")
     if visit_site:
         webbrowser.open("https://www.spoonacular.com/")
+    print("\n" + choice(greeting))
     
-    
+def save_to_file(filename, title, ingredients, instructions):
+    fulldatetime = datetime.now().strftime("%a %d %b, %H:%M %z")
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(f"\n{logo}\n\nRecipe Export - {fulldatetime}\n")
+        file.write("\nDietary Preference: ")
+        if vegan:
+            file.write("Vegan")
+        if vegetarian:
+            file.write("Vegetarian")
+        if len(exclusions) > 0:
+            file.write("\n\nExclusions: ")
+            for exclusion in exclusions:
+                exclusion_cap = exclusion.capitalize()
+                file.write(f"\n- {exclusion_cap}")
+        file.write(f"\n\nRecipe: {title}\n")
+        file.write("\nIngredients:\n")
+        for ingredient in ingredients:
+            file.write(f"- {ingredient}\n")
+        file.write("\nInstructions:\n")
+        file.write(f"\n{instructions}\n")
+        file.write("\nThanks for using ReciPy!\nData provided by Spoonacular API (www.spoonacular.com)")
+
 
 
 # <<< End of class, function, and API construction definitions. >>>
@@ -118,16 +139,16 @@ else:  # Provide a list of intolerances inclusive of meat options
 
 intolerances_input = str(input("\nOr else, just hit Enter if you have no intolerances.\n>>> ")).lower()  # Requires user input
 
-intolerances_map = {'a': 'grain', 
-                    'e': 'seafood', 
-                    'f': 'sulfite', 
-                    'g': 'gluten', 
-                    'h': 'shellfish', 
-                    'm': 'sesame', 
-                    'p': 'peanut', 
-                    's': 'soy', 
-                    't': 'tree-nut', 
-                    'w': 'wheat',       
+intolerances_map = {"a": "grain", 
+                    "e": "seafood", 
+                    "f": "sulfite", 
+                    "g": "gluten", 
+                    "h": "shellfish", 
+                    "m": "sesame", 
+                    "p": "peanut", 
+                    "s": "soy", 
+                    "t": "tree-nut", 
+                    "w": "wheat",       
                     }  # Map for shorthands
 
 exclusions = []  # Create empty `exclusions` list
@@ -143,7 +164,7 @@ if not dairy_ok:
 if not eggs_ok:
     exclusions.append("eggs")  # Append 'eggs' to exclusions list if answered 'n' to eggs in beginning
 
-exclusions_param = ','.join(exclusions)  # Set up the exclusion parameter for the API call
+exclusions_param = ",".join(exclusions)  # Set up the exclusion parameter for the API call
 # <<< End of User Preferences >>>
 
 # print(exclusions_param)  # Uncomment for debug output
@@ -176,7 +197,7 @@ if response.status_code != 200:  # Code 200 indicates success. Anything else, we
     print(f"Error fetching random recipe: Code - {response.status_code}")  # Verbose
     exit()
 
-recipe_id = response.json()['recipes'][0]['id']
+recipe_id = response.json()["recipes"][0]["id"]
 
 response = requests.get(DETAILED_RECIPE_URL.format(id=recipe_id), params={"apiKey": API_KEY})
 
@@ -190,7 +211,16 @@ recipe_info = response.json()
 
 print("\n")
 title, ingredients, instructions = extract_recipe_information(recipe_info)
+
 print("\n")
 display_recipe(title, ingredients, instructions)
+
+print("\n")
+if get_yes_no_input("Would you like to save this recipe as a text file?"):
+    current_datetime = datetime.now().strftime("%d-%m-%y-%H%M%z")
+    filename = f"ReciPy - {title} - {current_datetime}.txt"
+    save_to_file(filename, title, ingredients, instructions)
+    print(f"\nRecipe saved to {filename}.\n")
+
 end_greeting()
-input("\n\nPress Enter to exit...")
+input("\nPress [Enter] to exit...\n>>> ")
